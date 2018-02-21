010996960202a1a152a61870b032ec5f3f26f79d
Paul Lathrop <paul.lathrop@PLATHROP-ML1.local>
2018-02-21 16:31:39 -0700

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -39,17 +39,37 @@ UserSchema.methods.toJSON = function () {
   return _.pick(userObject, ['_id', 'email']);
 };
 
-UserSchema.methods.generateAuthToken = function () {
-  var user = this; //'this' keyword unavailable in arrow function hence 'function'
-  var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  UserSchema.methods.generateAuthToken = function () {
+    var user = this; //'this' keyword unavailable in arrow function hence 'function'
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
-user.tokens = user.tokens.concat([{access, token}]);
+  user.tokens = user.tokens.concat([{access, token}]);
 
-return user.save().then(() => {
+  return user.save().then(() => {
   return token;
-});
+  });
+};
 
+UserSchema.statics.findByToken = function(token){
+  var User = this;
+  var decoded;
+  //jwt.verify();
+
+  try{
+    decoded = jwt.verify(token, 'abc123');
+  } catch(e){
+    // return new Promise((resolve, reject) => {
+    //   reject(); //same as next line below
+    //});
+    return Promise.reject();
+
+  };
+  return User.findOne({ //error on lowecase user
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
 };
 
 var User = mongoose.model('User', UserSchema);
