53b68f85cd283af50b5f7544abc847ef2892a930
Paul Lathrop <paul.lathrop@PLATHROP-ML1.local>
2018-03-26 08:14:33 -0700

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,17 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test'){
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env == 'test'){
+  var config = require('./config.json');
+  var envConfig = config[env]; //use bracket for variable to access property
+
+  Object.keys(envConfig).forEach((key) =>{
+    process.env[key] = envConfig[key];
+  });
 }
+
+// if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test'){
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
